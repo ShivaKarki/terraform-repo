10eb454c2a6bbf330ae720b61f3840b926397b4b
Shiva Karki <62309351+ShivaKarki@users.noreply.github.com>
2022-06-09 11:59:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "0"
-  client_id       = "00000000-0000-0000-0000-000000000000"
-  client_secret   = 
-  tenant_id       = "00000000-0000-0000-0000-000000000000
+  subscription_id = "a54b6fd0-015e-4f77-a123-3ba3e93a64d8"
+  client_id       = "c58f588d-13dd-42a5-8975-366b9417bb67"
+  client_secret   = "40a54fad-1ebe-40aa-ad07-e82e4f9e8230"
+  tenant_id       = "4569bf05-cab0-4330-93f8-4e8563c99907"
 }
 
 resource "azurerm_resource_group" "rg" {
