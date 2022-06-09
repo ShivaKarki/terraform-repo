237d0be79b353f6ff23b136b7feeb1bf3f7705c3
Shiva Karki <62309351+ShivaKarki@users.noreply.github.com>
2022-06-09 12:08:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   features {}
   subscription_id = "a54b6fd0-015e-4f77-a123-3ba3e93a64d8"
   client_id       = "c58f588d-13dd-42a5-8975-366b9417bb67"
-  client_secret   = "40a54fad-1ebe-40aa-ad07-e82e4f9e8230"
+  client_secret   = "PYr8Q~71MYuZ8-Jbk3WSNYy-LDoqVHYQpqKe2dhm"
   tenant_id       = "4569bf05-cab0-4330-93f8-4e8563c99907"
 }
 
