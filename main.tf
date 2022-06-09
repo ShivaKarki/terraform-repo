fa071bb50561aba6615bbabe2294d82f92eca416
Shiva Karki <62309351+ShivaKarki@users.noreply.github.com>
2022-06-09 11:57:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "0"
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = 
+  tenant_id       = "00000000-0000-0000-0000-000000000000
 }
 
 resource "azurerm_resource_group" "rg" {
