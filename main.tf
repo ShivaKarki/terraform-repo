21f4b554fe3db29eb8dff22846749e78da530fc7
Shiva Karki <62309351+ShivaKarki@users.noreply.github.com>
2022-06-10 14:23:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,7 @@ terraform {
 }
 
 provider "azurerm" {
-         ARM_CLIENT_ID: "c58f588d-13dd-42a5-8975-366b9417bb67"
-         ARM_CLIENT_SECRET:  "PYr8Q~71MYuZ8-Jbk3WSNYy-LDoqVHYQpqKe2dhm"
-         ARM_SUBSCRIPTION_ID: "a54b6fd0-015e-4f77-a123-3ba3e93a64d8"
-         ARM_TENANT_ID: "4569bf05-cab0-4330-93f8-4e8563c99907"
+        
   features {}
   subscription_id = "a54b6fd0-015e-4f77-a123-3ba3e93a64d8"
   client_id       = "c58f588d-13dd-42a5-8975-366b9417bb67"
