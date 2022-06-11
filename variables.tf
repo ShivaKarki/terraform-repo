8361c02e354758a24eb0846ea021ba899cf9abbc
Shiva Karki <62309351+ShivaKarki@users.noreply.github.com>
2022-06-10 22:08:25 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "AZURE_AD_CLIENT_ID" {
+  type = string
+  
+  }
+
+  sensitive = true
+}
+
+
+variable "AZURE_AD_CLIENT_SECRET" {
+  type = string
+  
+  }
+
+  sensitive = true
+}
+
+
+
+variable "AZURE_SUBSCRIPTION_ID" {
+  type = string
+  
+  }
+
+  sensitive = true
+}
+
+
+
+variable "AZURE_AD_TENANT_ID" {
+  type = string
+  
+  }
+
+  sensitive = true
+}
