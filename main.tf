852abffd7a0940cdc738595a1ae09ebddf706266
Shiva Karki <62309351+ShivaKarki@users.noreply.github.com>
2022-06-10 20:05:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 # Configure the Azure provider
 terraform {
+  required_version = "~> 1.2.2"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -9,14 +10,10 @@ terraform {
 }
 
 provider "azurerm" {
-        
+   # skip provider rego because we are using a service principal with limited access to Azure
+  skip_provider_registration = "true"   
   features {}
-  subscription_id = "a54b6fd0-015e-4f77-a123-3ba3e93a64d8"
-  service_principal = "Shiva_sp"
-  object_id = "1818812e-7ef6-45ab-afcf-ac774b017597"
-  client_id       = "c58f588d-13dd-42a5-8975-366b9417bb67"
-  client_secret   = "PYr8Q~71MYuZ8-Jbk3WSNYy-LDoqVHYQpqKe2dhm"
-  tenant_id       = "4569bf05-cab0-4330-93f8-4e8563c99907"
+  
 }
 
 resource "azurerm_resource_group" "rg" {
