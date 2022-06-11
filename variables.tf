e2dc56a41b2c7fed7f410f21bb2be4563f75881c
Shiva Karki <62309351+ShivaKarki@users.noreply.github.com>
2022-06-10 22:14:06 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,11 @@
 variable "AZURE_AD_CLIENT_ID" {
   type = string
-  
-  }
-
   sensitive = true
 }
 
 
 variable "AZURE_AD_CLIENT_SECRET" {
   type = string
-  
-  }
-
   sensitive = true
 }
 
@@ -19,9 +13,6 @@ variable "AZURE_AD_CLIENT_SECRET" {
 
 variable "AZURE_SUBSCRIPTION_ID" {
   type = string
-  
-  }
-
   sensitive = true
 }
 
@@ -29,8 +20,5 @@ variable "AZURE_SUBSCRIPTION_ID" {
 
 variable "AZURE_AD_TENANT_ID" {
   type = string
-  
-  }
-
   sensitive = true
 }
